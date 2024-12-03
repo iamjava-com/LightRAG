aacfb9aed6a03e1ce39383a4c4d43d4f8f9cea32
chao.dong <chao.dong@iamjava.com>
2024-12-03 12:57:56 +0900

    pass kargs

--- lightrag/operate.py
+++ lightrag/operate.py
@@ -463,7 +463,7 @@ async def kg_query(
     use_model_func = global_config["llm_model_func"]
     kw_prompt_temp = PROMPTS["keywords_extraction"]
     kw_prompt = kw_prompt_temp.format(query=query, examples=examples, language=language)
-    result = await use_model_func(kw_prompt, asdict(query_param))
+    result = await use_model_func(kw_prompt, **asdict(query_param))
     logger.info("kw_prompt result:")
     print(result)
     try:
