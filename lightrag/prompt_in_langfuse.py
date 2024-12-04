3de60eb276f6c47af6a910f5e8414716601480fc
chao.dong <chao.dong@iamjava.com>
2024-12-04 09:51:04 +0900

    save prompts in langfuse

--- lightrag/prompt_in_langfuse.py
+++ lightrag/prompt_in_langfuse.py
@@ -2,40 +2,30 @@ from langfuse import Langfuse
 
 langfuse = Langfuse()
 
+def get_latest_prompt(prompt_name):
+    return langfuse.get_prompt(prompt_name, label="latest").prompt
+
 GRAPH_FIELD_SEP = "<SEP>"
 
-PROMPTS = {}
+PROMPTS = {
+    "DEFAULT_LANGUAGE": "English",
+    "DEFAULT_TUPLE_DELIMITER": "<|>",
+    "DEFAULT_RECORD_DELIMITER": "##",
+    "DEFAULT_COMPLETION_DELIMITER": "<|COMPLETE|>",
+    "process_tickers": ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"],
+    "DEFAULT_ENTITY_TYPES": [get_latest_prompt(f"DEFAULT_ENTITY_TYPES_{i}") for i in range(4)],
+    "entity_extraction": get_latest_prompt("entity_extraction"),
+    "entity_extraction_examples": [get_latest_prompt(f"entity_extraction_examples_{i}") for i in range(3)],
+    "summarize_entity_descriptions": get_latest_prompt("summarize_entity_descriptions"),
+    "entiti_continue_extraction": get_latest_prompt("entiti_continue_extraction"),
+    "entiti_if_loop_extraction": get_latest_prompt("entiti_if_loop_extraction"),
+    "fail_response": get_latest_prompt("fail_response"),
+    "rag_response": get_latest_prompt("rag_response"),
+    "keywords_extraction": get_latest_prompt("keywords_extraction"),
+    "keywords_extraction_examples": [get_latest_prompt(f"keywords_extraction_examples_{i}") for i in range(3)],
+    "naive_rag_response": get_latest_prompt("naive_rag_response")
+}
 
-PROMPTS["DEFAULT_LANGUAGE"] = "English"
-PROMPTS["DEFAULT_TUPLE_DELIMITER"] = "<|>"
-PROMPTS["DEFAULT_RECORD_DELIMITER"] = "##"
-PROMPTS["DEFAULT_COMPLETION_DELIMITER"] = "<|COMPLETE|>"
-PROMPTS["process_tickers"] = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"]
-
-PROMPTS["DEFAULT_ENTITY_TYPES"] = [langfuse.get_prompt(f"DEFAULT_ENTITY_TYPES_{i}") for i in range(0, 4)]
-
-PROMPTS["entity_extraction"] = langfuse.get_prompt("entity_extraction")
-
-PROMPTS["entity_extraction_examples"] = [langfuse.get_prompt(f"entity_extraction_examples_{i}") for i in range(0, 3)]
-
-PROMPTS[
-    "summarize_entity_descriptions"
-] = langfuse.get_prompt("summarize_entity_descriptions")
-
-PROMPTS[
-    "entiti_continue_extraction"
-] = langfuse.get_prompt("entiti_continue_extraction")
-
-PROMPTS[
-    "entiti_if_loop_extraction"
-] = langfuse.get_prompt("entiti_if_loop_extraction")
-
-PROMPTS["fail_response"] = langfuse.get_prompt("fail_response")
-
-PROMPTS["rag_response"] = langfuse.get_prompt("rag_response")
-
-PROMPTS["keywords_extraction"] = langfuse.get_prompt("keywords_extraction")
-
-PROMPTS["keywords_extraction_examples"] = [langfuse.get_prompt(f"keywords_extraction_examples_{i}") for i in
-                                           range(0, 3)]
-PROMPTS["naive_rag_response"] = langfuse.get_prompt("naive_rag_response")
+if __name__ == '__main__':
+    prompt = get_latest_prompt("naive_rag_response")
+    print(prompt)
